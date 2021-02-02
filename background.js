b8f50885566a16735e438774863ef245d6f656d9
Thomas Bonk <thomas@bonk.dev>
2021-02-02 14:29:55 +0100

    Reorder tabs

--- background.js
+++ background.js
@@ -1,22 +1,59 @@
-const {tabs, commands} = chrome;
-const {executeScript, get, query, update, onActivated} = tabs;
+const { tabs, commands } = chrome;
+const { executeScript, get, query, update, onActivated } = tabs;
 
-const selectActiveTab = callback => query({active: true, currentWindow: true}, ([activeTab]) => callback(activeTab));
+const selectActiveTab = (callback) =>
+  query({ active: true, currentWindow: true }, ([activeTab]) =>
+    callback(activeTab)
+  );
 
-const switchTab = distance => selectActiveTab(activeTabs => {
-    query({currentWindow: true}, tabs => {
-        const targetIndex = (activeTabs.index + distance) % tabs.length;
-        chrome.tabs.highlight({tabs: targetIndex >= 0 ? targetIndex : tabs.length - 1});
+let isStatefulNavigationActive = false;
+const switchTab = (distance) =>
+  query({ currentWindow: true }, (tabs) => {
+    const [activeTab] = tabs.filter(({ active }) => active);
+    const targetIndex = (activeTab.index + distance) % tabs.length;
+    chrome.tabs.highlight({
+      tabs: targetIndex >= 0 ? targetIndex : tabs.length - 1,
     });
-});
+    isStatefulNavigationActive = true;
+  });
 
-const executeCode = code => selectActiveTab(({tabId}) => executeScript(tabId, {code})); 
+const prependTab = (activeTab) => {
+  if (!activeTab.pinned) {
+    chrome.tabs.move(activeTab.id, { index: 0 });
+  }
+  isStatefulNavigationActive = false;
+};
 
-commands.onCommand.addListener(command => {
-    switch(command) {
-        case "switch-to-next-tab": switchTab(1); break;
-        case "switch-to-previous-tab": switchTab(-1); break;
-        case "history-back": executeCode('history.back();'); break;
-        case "history-forward": executeCode('history.forward();'); break;
-    }
+const prependActiveTab = () => selectActiveTab(prependTab);
+
+const prependTabById = (id) =>
+  !isStatefulNavigationActive && chrome.tabs.get(id, prependTab);
+
+const executeCode = (code) =>
+  selectActiveTab(({ tabId }) => executeScript(tabId, { code }));
+
+chrome.tabs.onHighlighted.addListener(({ tabIds }) =>
+  setTimeout(() => prependTabById(tabIds[0]), 350)
+);
+
+chrome.tabs.onActivated.addListener(({ tabId }) => prependTabById(tabId));
+
+commands.onCommand.addListener((command) => {
+  switch (command) {
+    case "switch-to-next-tab":
+      switchTab(1);
+      break;
+    case "switch-to-previous-tab":
+      switchTab(-1);
+      break;
+    case "prepend-active-tab":
+      prependActiveTab();
+      break;
+    case "history-back":
+      executeCode("history.back();");
+      break;
+    case "history-forward":
+      executeCode("history.forward();");
+      break;
+  }
 });
