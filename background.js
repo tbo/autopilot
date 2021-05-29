e83bacbc0641457209a1f2b44045d25733ce86e9
Thomas Bonk <thomas@bonk.dev>
2021-05-29 21:08:17 +0200

    Remove tab repositioning

--- background.js
+++ background.js
@@ -17,28 +17,8 @@ const switchTab = (distance) =>
     isStatefulNavigationActive = true;
   });
 
-const prependTab = (activeTab) => {
-  if (!activeTab.pinned) {
-    chrome.tabs.move(activeTab.id, { index: 0 });
-  }
-  isStatefulNavigationActive = false;
-};
-
-const prependActiveTab = () => selectActiveTab(prependTab);
-
-const prependTabById = (id) =>
-  !isStatefulNavigationActive && chrome.tabs.get(id, prependTab);
-
 const executeCode = (code) =>
   selectActiveTab(({ tabId }) => executeScript(tabId, { code }));
-
-// chrome.tabs.onHighlighted.addListener(({ tabIds }) =>
-//   setTimeout(() => prependTabById(tabIds[0]), 350)
-// );
-
-// chrome.tabs.onActivated.addListener(({ tabId }) => prependTabById(tabId));
-
-chrome.tabs.onCreated.addListener(({ id }) => prependTabById(id));
 
 commands.onCommand.addListener((command) => {
   switch (command) {
@@ -47,9 +27,6 @@ commands.onCommand.addListener((command) => {
       break;
     case "switch-to-previous-tab":
       switchTab(-1);
-      break;
-    case "prepend-active-tab":
-      prependActiveTab();
       break;
     case "history-back":
       executeCode("history.back();");
